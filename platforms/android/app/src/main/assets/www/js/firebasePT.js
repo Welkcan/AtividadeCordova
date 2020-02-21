5b1d7603090a3de0aaa54a404a71a228596fec57
Welkcan <welkcan@gmail.com>
2020-02-20 22:11:38 -0300

    first commit

---
+++ platforms/android/app/src/main/assets/www/js/firebasePT.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATB_BjBGUKT-karaxsUXKcIq-TinqGg8I",
+    authDomain: "cadastropt-54f13.firebaseapp.com",
+    databaseURL: "https://cadastropt-54f13.firebaseio.com",
+    projectId: "cadastropt-54f13",
+    storageBucket: "cadastropt-54f13.appspot.com",
+    messagingSenderId: "328752441467",
+    appId: "1:328752441467:web:59b0e1fb2872b5c5e7dade",
+    measurementId: "G-V3NBWC7H1R"
+};
+  
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
